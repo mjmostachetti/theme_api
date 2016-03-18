89a2f52bf297c5e1bf8f4d57b7099b5ac9c35c4c
Peter Ajtai <peter.ajtai@thinksolid.com>
2016-03-18 14:34:13 -0700

    Creating generalized middleware

--- app/index.js
+++ app/index.js
@@ -12,46 +12,92 @@ execSync('rm -r public/portal || echo "nothing to clean"', { stdio : 'inherit' }
 
 var app = express();
 app.use(express.static('public'));
+app.use(expressDynamicThemer({ basePath : '/portal/styles' }));
 
-// http://localhost:3000/portal/styles/proschools?color1=red&color2=blue&color3=purple
 
-app.get('/portal/styles/:customer/color1/:color1/color2/:color2/color3/:color3/style.css', function (req, res) {
-	var colorsObj = req.query,
-		customer = req.params.customer,
-        stylesheet = `$dark : ${req.params.color1}; 
-                $medium : ${req.params.color2}; 
-                $light : ${req.params.color3};
-                @import 'sass/style'`;
-		// assuming we get all three colors
+function expressDynamicThemer(options) {
+    // assumtions
+        // User has allready added the static middleware
+        // scss main file names mirror the requested styles file names
 
-    console.log(`stylesheet: ${stylesheet}`);
+    return function expressDynamicThemerMiddleware(req, res, next) {
 
-	sass.render({
-        data : stylesheet,
-        includePaths : [ 'sass/']
-	}, function(error, result) { // node-style callback from v3.0.0 onwards
-		console.log('error', error);
-		if(!error){
-			console.log('returnging');
-			postcss([ autoprefixer ]).process(result.css).then(function (postResult) {
-                var filename =  path.join('public',req.path.slice(1));
-				postResult.warnings().forEach(function (warn) {
-					console.warn(warn.toString());
-				});
+        var pathName= req.path;
 
-				res.writeHead(200, {
-					'Content-Type': 'text/css',
-					'Cache-Control': 'max-age=0'
-				});
+        if(path.extname(pathName) === '.css') {
+            var sassVars = _getScssVariables(pathName),
+                filename = path.basename(pathName, '.css'),
+                stylesheet = `${ sassVars } @import 'sass/${ filename }'`;
+            // assuming we get all three colors
 
-				res.end(postResult.css);
-                mkdirp.sync(path.dirname(filename));
-                fs.writeFileSync(filename, postResult.css);
-			});
-		}
-	});
-});
+            console.log(`stylesheet: ${stylesheet}`);
+
+            sass.render({
+                data : stylesheet,
+                includePaths : [ 'sass/']
+            }, function(error, result) { // node-style callback from v3.0.0 onwards
+                console.log('error', error);
+                if(!error){
+                    console.log('returnging');
+                    postcss([ autoprefixer ]).process(result.css).then(function (postResult) {
+                        var filename =  path.join('public',req.path.slice(1));
+                        postResult.warnings().forEach(function (warn) {
+                            console.warn(warn.toString());
+                        });
+
+                        res.writeHead(200, {
+                            'Content-Type': 'text/css',
+                            'Cache-Control': 'max-age=0'
+                        });
+
+                        res.end(postResult.css);
+                        mkdirp.sync(path.dirname(filename));
+                        fs.writeFileSync(filename, postResult.css);
+                    });
+                }
+            });
+        } else {
+            next();
+        }
+        // is file a css file?
+        // look at route, build obj from key value scss var pairs
+        // using the path besides the actual file name, using options.basePath and the final stylesheet name / extension.
+        // run scss like normal.
+    };
+
+    function _getScssVariables(pathString) {
+        var string = '',
+            name = true,
+            pathArray;
+
+        pathString = path.dirname(pathString);
+        pathString = pathString.replace(options.basePath, '').replace(/^\//,'');
+        pathArray = pathString.split('/');
+
+        pathArray.forEach(function(part) {
+            if (name) {
+                string += `$${ part } : `;
+            } else {
+                string += `${ part };\n`;
+            }
+            name = !name;
+        });
+
+        return string;
+    }
+}
+
+
+
 
 app.listen(3000, function () {
-	console.log('Test http://localhost:3000/portal/styles/proschools?color1=red&color2=blue&color3=purple')
+    console.log('Test http://localhost:3000/portal/styles/proschools?color1=red&color2=blue&color3=purple')
 });
+
+// http://localhost:3000/portal/styles/proschools?color1=red&color2=blue&color3=purple
+return;
+app.get('/portal/styles/:customer/color1/:color1/color2/:color2/color3/:color3/style.css', function (req, res) {
+
+
+});
+
