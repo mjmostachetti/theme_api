3851489b0029a9908aa30fd54fd95170dcfe68ad
Luke Chinworth <lukechinworth@gmail.com>
2016-03-24 14:14:15 -0700

    move getscss func into scope and add some options

--- index.js
+++ index.js
@@ -24,11 +24,12 @@ function expressDynamicThemer(options) {
             sassRoot = options.sassRoot || function() {
                 throw new Error('You must define options.sassRoot to use the sass-theme-api-middleware')
             },
+            publicDir = options.publicDir || '',
             includePaths = options.includePaths || [],
-            basePath = options.basePath || '';
+            urlBase = options.urlBase || '';
 
         if (path.extname(pathName) === '.css') {
-            var sassVars = _getScssVariables(pathName, basePath),
+            var sassVars = _getScssVariables(pathName),
                 filename = path.basename(pathName, '.css'),
                 stylesheet = `${ sassVars } @import '${ sassRoot }/${ filename }'`;
 
@@ -36,50 +37,54 @@ function expressDynamicThemer(options) {
                 data : stylesheet,
                 includePaths : includePaths
             }, function(error, result) {
-                if(!error){
-                    postcss([ autoprefixer ]).process(result.css).then(function (postResult) {
-                        var filename =  path.join('public',req.path.slice(1));
-
-                        postResult.warnings().forEach(function (warn) {
-                            console.warn(warn.toString());
+                if (error) {
+                    console.log('sass render error: ', error);
+                } else {
+                    postcss([ autoprefixer ]).process(result.css)
+                        .then(function(postResult) {
+                            var filename =  path.join(publicDir,pathName.slice(1));
+    
+                            postResult.warnings().forEach(function(warn) {
+                                console.warn(warn.toString());
+                            });
+    
+                            res.writeHead(200, {
+                                'Content-Type': 'text/css',
+                                'Cache-Control': 'max-age=0'
+                            });
+    
+                            res.end(postResult.css);
+    
+                            // cache file
+                            mkdirp.sync(path.dirname(filename));
+                            fs.writeFileSync(filename, postResult.css);
                         });
-
-                        res.writeHead(200, {
-                            'Content-Type': 'text/css',
-                            'Cache-Control': 'max-age=0'
-                        });
-
-                        res.end(postResult.css);
-
-                        // cache file
-                        mkdirp.sync(path.dirname(filename));
-                        fs.writeFileSync(filename, postResult.css);
-                    });
                 }
             });
         } else {
             next();
         }
+
+        function _getScssVariables(pathString) {
+            var string = '',
+                name = true,
+                pathArray;
+
+            pathString = path.dirname(pathString);
+            pathString = pathString.replace(urlBase, '').replace(/^\//,'');
+            pathArray = pathString.split('/');
+
+            pathArray.forEach(function(part) {
+                if (name) {
+                    string += `$${ part } : `;
+                } else {
+                    string += `${ decodeURIComponent(part) };\n`;
+                }
+                name = !name;
+            });
+
+            return string;
+        }
     };
 }
 
-function _getScssVariables(pathString, basePath) {
-    var string = '',
-        name = true,
-        pathArray;
-
-    pathString = path.dirname(pathString);
-    pathString = pathString.replace(basePath, '').replace(/^\//,'');
-    pathArray = pathString.split('/');
-
-    pathArray.forEach(function(part) {
-        if (name) {
-            string += `$${ part } : `;
-        } else {
-            string += `${ decodeURIComponent(part) };\n`;
-        }
-        name = !name;
-    });
-
-    return string;
-}
